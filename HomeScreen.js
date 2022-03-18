f2180e1105ac149208080e445c01652d3b8a19c1
ArulBhardwaj <88354537+ArulBhardwaj@users.noreply.github.com>
2022-03-18 16:08:54 -0700

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Header,
+  Image,
+} from 'react-native';
+
+import AppHeader from '../components/AppHeader';
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <AppHeader />
+      </View>
+    );
+  }
+}
